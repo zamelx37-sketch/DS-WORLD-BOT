27acefb3a862cb353b45021a9de4044980156c01
zamelx37-sketch <zamelx37@gmail.com>
2026-03-14 15:14:12 +0000

    Added Info button to panel

--- index.js
+++ index.js
@@ -159,14 +159,13 @@ client.on("interactionCreate", async interaction => {
   const locked = vc.permissionsFor(interaction.guild.roles.everyone).has("Connect") ? "No" : "Yes";
 
   const infoEmbed = new EmbedBuilder()
-    .setTitle("✦ 𝒱𝒪𝐼𝒞𝐸 𝒞𝐻𝒜𝒩𝒩𝐸𝐿 𝐼𝒩𝐹𝒪 ✦")
+    .setTitle(" 𝒱𝒪𝐼𝒞𝐸 𝒞𝐻𝒜𝒩𝒩𝐸𝐿 𝐼𝒩𝐹𝒪  ")
     .setColor(0x9b59b6) // لون أنيق بنفسجي
     .setThumbnail("https://cdn.discordapp.com/emojis/1482387316088770681.png?size=96&quality=lossless")
-    .setDescription("╔══ ✦・✦・✦ ══╗\n♡ 𝒟𝒮 𝒲𝒪𝑅𝐿𝐷 𝒫𝒜𝒩𝐸𝐿 ♡\n╚══ ✦・✦・✦ ══╝")
-    .addFields(
-      { name: "𝒪𝓌𝓃𝑒𝓇 👑", value: `@${owner}`, inline: true },
-      { name: "𝒩𝒶𝓂𝑒 📛", value: name, inline: true },
-      { name: "𝐿𝒾𝓂𝒾𝓉 👥", value: limit, inline: true },
+    .setDescription("\n♡ 𝒟𝒮 𝒲𝒪𝑅𝐿𝐷 𝒫𝒜𝒩𝐸𝐿 ♡\    .addFields(
+      { name: "𝒪𝓌𝓃𝑒𝓇👑", value: `@${owner}`, inline: true },
+      { name: "𝒩𝒶𝓂𝑒📛", value: name, inline: true },
+      { name: "𝐿𝒾𝓂𝒾𝓉👥", value: limit, inline: true },
       { name: "𝒞𝓇𝑒𝒶𝓉𝑒𝒹 𝒶𝓉 ⏰", value: createdAt, inline: false },
       { name: "𝐻𝒾𝒹𝒹𝑒𝓃 🙈", value: hidden, inline: true },
       { name: "𝐿𝑜𝒸𝓀𝑒𝒹 🔒", value: locked, inline: true }
